2b176e5bc99cc5702216d8ac40d194d0b69287e2
Jerber <xion0597@umn.edu>
2021-01-07 23:50:58 -0600

    adding unity files

---
+++ Assets/enemy1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemy1 : MonoBehaviour {
+
+    public GameObject deathEffect;
+
+    public float health = 4f;
+
+    public static int EnemiesAlive = 0;
+
+    void Start() {
+        EnemiesAlive++;
+    }
+
+    void OnCollisionEnter2D (Collision2D colInfo) {
+        if (colInfo.relativeVelocity.magnitude > health) {
+            Die();
+        }
+        
+    }
+
+    void Die() {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+            Debug.Log("Level Won");
+
+        Destroy(gameObject);
+    }
+
+}
